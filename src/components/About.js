e096ac6e9ce6b415bd9b548e1b5f368a7cb5b729
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-07-17 23:37:03 +0300

    Done.

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-function About() {
+function About(bio) {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <a href=""></a>
     </div>
   );
 }
